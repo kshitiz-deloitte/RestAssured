eaac8b7ba6e14e6b69e232147f0fc9041f0b1036
kshitiz-deloitte <101085691+kshitiz-deloitte@users.noreply.github.com>
2022-04-04 13:51:25 +0530

    Feature added for Adding the data from Excel

---
+++ src/test/java/MainAssignment/User.java
@@ -0,0 +1,13 @@
+package MainAssignment;
+
+public class User {
+    private String token;
+
+    public String getToken() {
+        return token;
+    }
+
+    public void setToken(String token) {
+        this.token = token;
+    }
+}
